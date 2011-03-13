20f88dd1cac4c77e6845699148a4311f54bf05af
beloblotskiy@gmail.com <beloblotskiy@gmail.com@1342313e-8ac7-cad2-1676-26ca36c24181>
2011-03-13 20:04:33 +0000

    Sample python scripts

--- trunk/scripts/tools_samples/sample_script.py
+++ trunk/scripts/tools_samples/sample_script.py
@@ -1,2 +1,8 @@
-print "test!"
+import sys
+
+print "Sample script, called from SciTE tools menu..."
+print "Command line arguments:"
+for i in range(len(sys.argv)):
+	print "  Parameter #%s=%s" % (i, sys.argv[i])
+
 exit(0)
