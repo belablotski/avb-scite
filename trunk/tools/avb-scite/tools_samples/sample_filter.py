1356184ba0872b6981cc4214d22854606213420d
beloblotskiy <beloblotskiy@1342313e-8ac7-cad2-1676-26ca36c24181>
2011-03-16 13:21:28 +0000

    Small changes into scripts

--- trunk/tools/avb-scite/tools_samples/sample_filter.py
+++ trunk/tools/avb-scite/tools_samples/sample_filter.py
@@ -2,6 +2,7 @@ import sys
 
 
 print "Sample filter script, called from SciTE tools menu..."
+print "$(SelectionStartColumn) $(SelectionStartLine) $(SelectionEndColumn) $(SelectionEndLine)"
 print "Command line arguments:"
 for i in range(len(sys.argv)):
 	print "  Parameter #%s=%s" % (i, sys.argv[i])
