b9756fd222f46e59345dbe561c07c957715b27fc
beloblotskiy <beloblotskiy@1342313e-8ac7-cad2-1676-26ca36c24181>
2011-03-16 13:16:14 +0000

    Small changes into scripts

---
+++ trunk/tools/avb-scite/tools_samples/sample_filter_replace_sel.py
@@ -0,0 +1,9 @@
+import sys
+
+
+
+selection = sys.stdin.read()
+result = '/* ' + selection + ' */'
+sys.stdout.write(result)
+
+exit(0)
---
+++ trunk/tools/avb-scite/tools_samples/sample_script.py
@@ -0,0 +1,9 @@
+import sys
+
+
+print "Sample script, called from SciTE tools menu..."
+print "Command line arguments:"
+for i in range(len(sys.argv)):
+	print "  Parameter #%s=%s" % (i, sys.argv[i])
+
+exit(0)
