891420d254faf55ae2cfd3d36c5ff96d6557e2f0
beloblotskiy <beloblotskiy@1342313e-8ac7-cad2-1676-26ca36c24181>
2011-04-06 14:24:53 +0000

    winbatch runner tool

--- tools/avb-scite/tools_pack/winbatch.py
+++ tools/avb-scite/tools_pack/winbatch.py
@@ -2,3 +2,19 @@
 Windows batch-files launcer.
 """
 
+import avb_tools_conf
+import io, sys, os, os.path
+from avb_tools.core.basetools import TempFileTool
+from avb_tools.core.exceptions import EmptyInputException
+from avb_tools.cmd.winbatch import CmdWinBatchTool
+
+if __name__ == '__main__':
+    print(sys.argv)
+    exit()
+
+    x = sys.stdin.read()
+    if x == '@':
+        raise EmptyInputException()
+    tool = CmdWinBatchTool(io.StringIO(x[1:]), sys.stdout)
+    tool.run()
+
