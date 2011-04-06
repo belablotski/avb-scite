c0f718da3a9c4aa330b470b59174ab4aa7caff7b
beloblotskiy <beloblotskiy@1342313e-8ac7-cad2-1676-26ca36c24181>
2011-04-06 12:54:12 +0000

    sqlplus runner tool

--- tools/avb-scite/tools_pack/sqlplus.py
+++ tools/avb-scite/tools_pack/sqlplus.py
@@ -5,19 +5,14 @@ Oracle SQL*Plus launcher.
 import avb_tools_conf
 import io, sys, os, os.path
 from avb_tools.core.basetools import TempFileTool
-
-#temp_file_tool = TempFileTool(io.StringIO("select * from dual;"), sys.stdout)
-#temp_file_tool.run()
+from avb_tools.core.exceptions import EmptyInputException
 
 class SqlPlusTool(TempFileTool):
     def __init__(self):
         x = sys.stdin.read()
-        super(SqlPlusTool, self).__init__(io.StringIO(x), sys.stdout)
+        if x == '@':
+            raise EmptyInputException()
+        super(SqlPlusTool, self).__init__(io.StringIO(x[1:]), sys.stdout)
 
-#tool = SqlPlusTool()
-#tool.run()
-
-print("xxx")
-x = sys.stdin.read()
-print(x)
-
+tool = SqlPlusTool()
+tool.run()
